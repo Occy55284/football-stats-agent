f6ec34e806a8fbba0d8052462e33a6f6d65a03f8
Occy55284 <grahammark55284@gmail.com>
2026-03-21 15:40:08 +0000

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
-      <h1>Football Stats Agent</h1>
-      <p>The app is now running on Next.js.</p>
-    </main>
-  );
-}
